e6e757748a6998e11d9b307848ea254dbe14bf63
Davide <d.sadotti@gmail.com>
2021-03-09 14:58:42 +0100

    Ho inserito qualche controllo

--- js/script.js
+++ js/script.js
@@ -6,13 +6,23 @@ while(numPc.length < 16){
     numPc.push(numRandom);
   }
 }
+console.log(numPc);
+
 
 // Faccio inserire all'utente 16 numeri diversi
 var numUser = [];
-
-while(numUser.length < 16){
-  var number = parseInt(prompt("Inserisci un numero univoco da 1 a 50 per 16 volte. Fino ad ora hai inserito: " + numUser.length + "numeri!"));
+var lost = false;
+// Creo un cilco while per far inserire i numeri all'utente e per controllare se ha perso
+while(numUser.length < 16 && lost == false){
+  // Faccio inserire un numero casuale
+  var number = parseInt(prompt("Inserisci un numero univoco da 1 a 50 per 16 volte. Fino ad ora hai inserito: " + numUser.length + " numeri!"));
+  // Controllo se quel numero è già stato inserito
   if(numUser.indexOf(number) == -1){
     numUser.push(number);
+  }
+  // Controllo se quel numero è un numero vietato
+  if(numPc.includes(number)){
+    lost = true;
+    alert("Hai perso sfigato!")
   }
 }
